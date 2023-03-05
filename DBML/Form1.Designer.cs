b8fb75b4bd5eefc515b57cb6586fcc154cda53f1
mikefirefly <13250690+mikefirefly@users.noreply.github.com>
2023-03-05 21:25:37 +0100

    Add files via upload

--- DBML/Form1.Designer.cs
+++ DBML/Form1.Designer.cs
@@ -45,6 +45,7 @@
             this.btnSave = new System.Windows.Forms.Button();
             this.btnOpenPathInExplorer = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -61,7 +62,7 @@
             this.lbEntryList.ItemHeight = 23;
             this.lbEntryList.Location = new System.Drawing.Point(9, 12);
             this.lbEntryList.Name = "lbEntryList";
-            this.lbEntryList.Size = new System.Drawing.Size(338, 372);
+            this.lbEntryList.Size = new System.Drawing.Size(421, 395);
             this.lbEntryList.TabIndex = 0;
             this.lbEntryList.ValueMember = "FriendlyName";
             this.lbEntryList.SelectedIndexChanged += new System.EventHandler(this.lbEntryList_SelectedIndexChanged);
@@ -76,12 +77,12 @@
             this.tbDOSBoxConfig.BackColor = System.Drawing.Color.White;
             this.tbDOSBoxConfig.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.tbDOSBoxConfig.Enabled = false;
-            this.tbDOSBoxConfig.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbDOSBoxConfig.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.tbDOSBoxConfig.ForeColor = System.Drawing.Color.Black;
-            this.tbDOSBoxConfig.Location = new System.Drawing.Point(366, 194);
+            this.tbDOSBoxConfig.Location = new System.Drawing.Point(446, 194);
             this.tbDOSBoxConfig.Multiline = true;
             this.tbDOSBoxConfig.Name = "tbDOSBoxConfig";
-            this.tbDOSBoxConfig.Size = new System.Drawing.Size(360, 192);
+            this.tbDOSBoxConfig.Size = new System.Drawing.Size(410, 260);
             this.tbDOSBoxConfig.TabIndex = 1;
             this.tbDOSBoxConfig.TextChanged += new System.EventHandler(this.tbDOSBoxConfig_TextChanged);
             // 
@@ -91,9 +92,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbPath.Enabled = false;
-            this.tbPath.Location = new System.Drawing.Point(366, 84);
+            this.tbPath.Location = new System.Drawing.Point(446, 84);
             this.tbPath.Name = "tbPath";
-            this.tbPath.Size = new System.Drawing.Size(324, 20);
+            this.tbPath.Size = new System.Drawing.Size(374, 20);
             this.tbPath.TabIndex = 2;
             this.tbPath.TextChanged += new System.EventHandler(this.tbPath_TextChanged);
             // 
@@ -103,9 +104,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbLaunch.Enabled = false;
-            this.tbLaunch.Location = new System.Drawing.Point(366, 133);
+            this.tbLaunch.Location = new System.Drawing.Point(446, 133);
             this.tbLaunch.Name = "tbLaunch";
-            this.tbLaunch.Size = new System.Drawing.Size(360, 20);
+            this.tbLaunch.Size = new System.Drawing.Size(410, 20);
             this.tbLaunch.TabIndex = 2;
             this.tbLaunch.TextChanged += new System.EventHandler(this.tbLaunch_TextChanged);
             // 
@@ -113,7 +114,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(363, 178);
+            this.label1.Location = new System.Drawing.Point(443, 178);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(145, 13);
             this.label1.TabIndex = 3;
@@ -123,7 +124,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(363, 68);
+            this.label2.Location = new System.Drawing.Point(443, 68);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(100, 13);
             this.label2.TabIndex = 3;
@@ -133,7 +134,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(363, 117);
+            this.label3.Location = new System.Drawing.Point(443, 117);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(115, 13);
             this.label3.TabIndex = 3;
@@ -145,7 +146,7 @@
             this.btnStart32.Enabled = false;
             this.btnStart32.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnStart32.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStart32.Location = new System.Drawing.Point(264, 398);
+            this.btnStart32.Location = new System.Drawing.Point(245, 462);
             this.btnStart32.Name = "btnStart32";
             this.btnStart32.Size = new System.Drawing.Size(83, 39);
             this.btnStart32.TabIndex = 4;
@@ -164,7 +165,7 @@
             this.btnDelete.BackColor = System.Drawing.SystemColors.Control;
             this.btnDelete.Enabled = false;
             this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnDelete.Location = new System.Drawing.Point(9, 396);
+            this.btnDelete.Location = new System.Drawing.Point(117, 460);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(83, 41);
             this.btnDelete.TabIndex = 4;
@@ -178,9 +179,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbFriendlyName.Enabled = false;
-            this.tbFriendlyName.Location = new System.Drawing.Point(366, 36);
+            this.tbFriendlyName.Location = new System.Drawing.Point(446, 36);
             this.tbFriendlyName.Name = "tbFriendlyName";
-            this.tbFriendlyName.Size = new System.Drawing.Size(360, 20);
+            this.tbFriendlyName.Size = new System.Drawing.Size(410, 20);
             this.tbFriendlyName.TabIndex = 5;
             this.tbFriendlyName.TextChanged += new System.EventHandler(this.tbFriendlyName_TextChanged);
             // 
@@ -188,7 +189,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(363, 20);
+            this.label4.Location = new System.Drawing.Point(443, 20);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(34, 13);
             this.label4.TabIndex = 3;
@@ -196,14 +197,13 @@
             // 
             // btnSave
             // 
-            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnSave.Enabled = false;
             this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnSave.Location = new System.Drawing.Point(279, 384);
+            this.btnSave.Location = new System.Drawing.Point(279, 446);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(88, 27);
+            this.btnSave.Size = new System.Drawing.Size(138, 29);
             this.btnSave.TabIndex = 4;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = false;
@@ -211,13 +211,13 @@
             // 
             // btnOpenPathInExplorer
             // 
-            this.btnOpenPathInExplorer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.btnOpenPathInExplorer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnOpenPathInExplorer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnOpenPathInExplorer.Enabled = false;
-            this.btnOpenPathInExplorer.Location = new System.Drawing.Point(338, 69);
+            this.btnOpenPathInExplorer.Location = new System.Drawing.Point(384, 68);
             this.btnOpenPathInExplorer.Name = "btnOpenPathInExplorer";
-            this.btnOpenPathInExplorer.Size = new System.Drawing.Size(30, 21);
+            this.btnOpenPathInExplorer.Size = new System.Drawing.Size(34, 22);
             this.btnOpenPathInExplorer.TabIndex = 7;
             this.btnOpenPathInExplorer.Text = "...";
             this.btnOpenPathInExplorer.UseVisualStyleBackColor = true;
@@ -225,20 +225,34 @@
             // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel1.Controls.Add(this.btnOpenPathInExplorer);
             this.panel1.Controls.Add(this.btnSave);
-            this.panel1.Location = new System.Drawing.Point(356, 12);
+            this.panel1.Location = new System.Drawing.Point(436, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(377, 425);
+            this.panel1.Size = new System.Drawing.Size(427, 487);
             this.panel1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 414);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(220, 39);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "F1 = Set/unset favourite\r\nDelete = remove entry\r\nShift+Delete = remove entry and " +
+    "delete folder";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(739, 449);
+            this.ClientSize = new System.Drawing.Size(869, 511);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.tbFriendlyName);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnStart32);
@@ -281,6 +295,7 @@
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnOpenPathInExplorer;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
     }
 }
 
