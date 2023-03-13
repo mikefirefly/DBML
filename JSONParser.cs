f5daf5db3666fc5ec8ba84856817c51540cd6ee3
mikefirefly <13250690+mikefirefly@users.noreply.github.com>
2023-03-13 15:44:48 +0100

    Update JSONParser.cs

--- JSONParser.cs
+++ JSONParser.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization;
 using System.Text;
-
+asdasdasdasdasdasdasd
 namespace TinyJson
 {
     // Really simple JSON parser in ~300 lines
